1eaf63f723064d6129dab14c0fbd5408adaa891d
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2018-11-20 09:00:02 +0100

    Try to fix travis generating snapshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,11 @@
-import { configure, addDecorator } from '@storybook/react';
-import { initScreenshot } from 'storybook-chrome-screenshot';
+import { configure, addDecorator } from "@storybook/react";
+import { initScreenshot } from "storybook-chrome-screenshot";
 
 // Initialize `storybook-chrome-screenshot`
 addDecorator(initScreenshot());
 
 // Load stories
-const req = require.context('../src', true, /\.stories\.js$/);
+const req = require.context("../src", true, /\.stories\.js$/);
 
 configure(() => {
   req.keys().forEach(filename => req(filename));
