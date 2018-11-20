cce68a4d987fbc765aa712cf0e7043a827340302
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2018-11-20 17:40:38 +0100

    Test different styles

--- src/Button.js
+++ src/Button.js
@@ -5,15 +5,15 @@ export const Button = ({ children, primary }) => (
     style={{
       display: "inline-block",
       padding: "0 48px",
-      height: 50,
-      borderRadius: 50,
+      height: "50px",
+      borderRadius: "50px",
       lineHeight: "50px",
       borderStyle: "solid",
       borderWidth: 1,
       borderColor: primary ? "#00c4a7" : "#adadad",
       background: primary ? "#00c4a7" : "#fff",
       color: primary ? "#fff" : "#363636",
-      fontSize: 14,
+      fontSize: "14px",
       fontFamily: "sans-serif"
       // textRendering: "optimizeLegibility"
     }}
