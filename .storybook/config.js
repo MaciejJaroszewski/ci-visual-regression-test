394d72f3b053e8ab246c97f7386b8cd200b36594
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2018-11-20 19:49:46 +0100

    Remove font dependency in visual test

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,28 @@
 import { configure, addDecorator } from "@storybook/react";
 import { initScreenshot } from "storybook-chrome-screenshot";
+import { setScreenshotOptions } from "storybook-chrome-screenshot";
 
 // Initialize `storybook-chrome-screenshot`
 addDecorator(initScreenshot());
+setScreenshotOptions({
+  waitFor: "fontLoading"
+});
 
 // Load stories
 const req = require.context("../src", true, /\.stories\.js$/);
+
+function fontLoading() {
+  const loaded = () =>
+    !!document.querySelector('link[rel="preload"][loaded="loaded"]');
+  if (loaded()) return Promise.resolve();
+  return new Promise((resolve, reject) => {
+    const id = setInterval(() => {
+      if (!loaded()) return;
+      clearInterval(id);
+      resolve();
+    }, 50);
+  });
+}
 
 configure(() => {
   req.keys().forEach(filename => req(filename));
