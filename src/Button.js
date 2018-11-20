9b613e428f67c1e10325cb308af7863f7eaa7729
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2018-11-20 17:50:32 +0100

    Update button styles

--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,7 @@
 import React from "react";
 
+import "./button.css";
+
 export const Button = ({ children, primary }) => (
-  <button
-    style={{
-      display: "inline-block",
-      padding: "0 48px",
-      height: "50px",
-      borderRadius: "50px",
-      lineHeight: "50px",
-      borderStyle: "solid",
-      borderWidth: 1,
-      borderColor: primary ? "#00c4a7" : "#adadad",
-      background: primary ? "#00c4a7" : "#fff",
-      color: primary ? "#fff" : "#363636",
-      fontSize: "14px",
-      fontFamily: "sans-serif"
-      // textRendering: "optimizeLegibility"
-    }}
-  >
-    {children}
-  </button>
+  <button className={primary ? "primary" : ""}>{children}</button>
 );
