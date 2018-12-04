2eee5fb4ce08b55489749247164957ad587ce2ee
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2018-12-04 14:43:32 +0100

    Test different package to visual testing

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,28 +1,8 @@
-import { configure, addDecorator } from "@storybook/react";
-import { initScreenshot } from "storybook-chrome-screenshot";
-import { setScreenshotOptions } from "storybook-chrome-screenshot";
-
-// Initialize `storybook-chrome-screenshot`
-addDecorator(initScreenshot());
-setScreenshotOptions({
-  waitFor: "fontLoading"
-});
+import { configure, addDecorator, getStorybook } from "@storybook/react";
+import 'loki/configure-react';
 
 // Load stories
 const req = require.context("../src", true, /\.stories\.js$/);
-
-function fontLoading() {
-  const loaded = () =>
-    !!document.querySelector('link[rel="preload"][loaded="loaded"]');
-  if (loaded()) return Promise.resolve();
-  return new Promise((resolve, reject) => {
-    const id = setInterval(() => {
-      if (!loaded()) return;
-      clearInterval(id);
-      resolve();
-    }, 50);
-  });
-}
 
 configure(() => {
   req.keys().forEach(filename => req(filename));
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,21 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { withScreenshot } from "storybook-chrome-screenshot";
 import { Button } from "./Button";
 
 storiesOf("Button", module)
-  .addDecorator(
-    withScreenshot({
-      delay: 1000,
-      viewport: {
-        width: 1024,
-        height: 768,
-        deviceScaleFactor: 1,
-        isMobile: false,
-        hasTouch: false,
-        isLandscape: false
-      }
-    })
-  )
   .add("with default style", () => <Button>TEST</Button>)
   .add("with primary style", () => <Button primary>TEST</Button>);
