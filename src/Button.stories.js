c7a8fe533fba96fb6a478c97d399f8c93af24172
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2018-12-05 12:35:45 +0100

    Leave only one snapshot testing

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -5,5 +5,5 @@ import { Button } from "./Button";
 storiesOf("Button", module)
   .add("with default style", () => <Button>TEST</Button>)
 
-storiesOf("Button primary", module)
-  .add("with primary style", () => <Button primary>TEST PRIMARY</Button>);
+// storiesOf("Button primary", module)
+//   .add("with primary style", () => <Button primary>TEST PRIMARY</Button>);
