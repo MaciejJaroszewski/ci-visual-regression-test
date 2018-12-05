7951b641be7de56c9f758c8d06ca09038eae590e
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2018-12-05 09:07:06 +0100

    Divide stories

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -4,4 +4,6 @@ import { Button } from "./Button";
 
 storiesOf("Button", module)
   .add("with default style", () => <Button>TEST</Button>)
+
+storiesOf("Button primary", module)
   .add("with primary style", () => <Button primary>TEST PRIMARY</Button>);
