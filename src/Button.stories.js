7696a0108f5cc8fbe33928e13e5e715cc33118a0
Maciej Jaroszewski <maciej.jaroszewski@netguru.pl>
2019-01-17 12:35:04 +0100

    Test CI version of loki repo

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,9 +1,19 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { Button } from "./Button";
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Button } from './Button'
 
-storiesOf("Button", module)
-  .add("with default style", () => <Button>TEST</Button>)
+storiesOf('Button', module).add('with default style', () => <Button>TEST</Button>)
 
-// storiesOf("Button primary", module)
-//   .add("with primary style", () => <Button primary>TEST PRIMARY</Button>);
+storiesOf('Button primary', module).add('with primary style', () => <Button primary>TEST PRIMARY</Button>)
+
+storiesOf('Button2', module).add('with default style', () => <Button>TEST</Button>)
+
+storiesOf('Button primary2', module).add('with primary style', () => <Button primary>TEST PRIMARY</Button>)
+
+storiesOf('Button3', module).add('with default style', () => <Button>TEST</Button>)
+
+storiesOf('Button primary3', module).add('with primary style', () => <Button primary>TEST PRIMARY</Button>)
+
+storiesOf('Button4', module).add('with default style', () => <Button>TEST</Button>)
+
+storiesOf('Button primary4', module).add('with primary style', () => <Button primary>TEST PRIMARY</Button>)
